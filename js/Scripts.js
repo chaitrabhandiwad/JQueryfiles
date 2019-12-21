2cc39e2090dc8cde7d5e436d129dc6326cae0de9
chaitrabhandiwad <chaitrabhandiwad@users.noreply.github.com>
2019-12-21 22:52:15 +0100

    Jquery Brewery using API revised

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,155 +1,137 @@
-var breweryRepository = (function($) {    //Start of IIFE
-  var repository = [];
-  var apiUrl = 'https://api.openbrewerydb.org/breweries';
-  var $modalContainer = $('#modal-container');
-  var $breweryList = $('ul');
+var breweryRepository = (function () { /*Pokedex Object Array Placed Inside IIFE*/
+ "use strict";
+var repository = [ ];
 
-  //function to add new brewery
-  function add(brewery){
-    //Must be an 'object' type
-  if (typeof brewery !== 'object') {
-    return 'Not a valid input'
-  }else{
-  repository.push(brewery);
+ var apiUrl = 'https://api.openbrewerydb.org/breweries';
+ function addListItem(brewery){
+        var brewlist = $('.brewery-list');  /* JQuery List Item & Button Tags Together with CSS-Class Styles Created */
+        var listitem = $('<li>');
+        $(brewlist).append(listitem);
+        var btn = $('<button>');
+        $(listitem).append(btn);
+        $(btn).text(brewery.name);
+        $(btn).addClass('brew-btn');
+        $(listitem).addClass('brewitem');
+        btn.on('click', function(event) { /*JQuery Click Button Event Listener Used To Display showDetails Function Properties */
+        showDetails(brewery);
+    });
   }
-}
 
-//Function to pull all Pokemon data
-function getAll() {
-  return repository;
-}
+ function showDetails(item) {
+    breweryRepository.loadDetails(item).then(function () {
+    showModal(item);
+    });
+  }
+function add(name) { /*Add Additional Pokemon Attributes To Object Array*/
+      repository.push(name);
 
-//function to add brewery object
-function addListItem(brewery) {
-  var brewerylist = $('.Brewery-list');
-    var $listItem = $('<li></li>');
-    $breweryList.append($listItem);
-    var $button = $('<button class="brewery-name">' + brewery.name + '</button>');
-    $listItem.append($button);
-    $button.on('click', function() {
-      showDetails(brewery)
-    })
+  }
+
+  function catchAll() { /* Function Used To Return Pokedex Object Array*/
+      return repository;
   }
 
   function loadList() {
-    return $.ajax(apiUrl, {dataType: 'json'}).then(function (item) { /* Replaced Fectch With Ajax*/
-
-    $.each(item.results, function(index, item){
+    return $.ajax(apiUrl, {dataType: 'json'}).then(function (response) { /* Replaced Fectch With Ajax*/
+     console.log(response);
+    $.each(response.results, function(index, item){
         var brewery = {
           name: item.name,
-          detailsUrl: item.url
+          detailsState: item.state,
+          detailsCity: item.city
         };
         add(brewery);
       });
-    }).catch(function (error) {
-      /*Load Functions Set In Order To Retrieve Data From Brewery API*/
+    }).catch(function (error) { /*Load Functions Set In Order To Retrieve Data From Pokemon API*/
       console.error(error);
     });
   }
-  //function to load the brewery details
   function loadDetails(item) {
-      var url = item.detailsUrl;
-      return $.ajax(url, {dataType: 'json'}).then(function(details) {
-        //item.websiteUrl = details.website_url;
-        item.state = details.state;
+    //var url = item.detailsUrl;
+    return $.ajax(apiUrl).then(function (details) { /* Replaced Fectch With Ajax*/
+        //item.imageUrl = details.sprites.front_default;
         item.city = details.city;
-        //item.types = Object.keys(details.types);
-      }).catch(function(e) {
-        console.error(e);
-      })
-    }
-
-
-    /*Model Definition With Jquery Start*/
-  function showModal(item){
-
-      var modalContainer = $('#modal-container');
-        $(modalContainer).text('');
-        $(modalContainer).addClass('is-visible');
-
-      var brewerymodal = $('<div>');
-        $(brewerymodal).addClass('modal');
-        $(modalContainer).append(brewerymodal);
-
-      var $closeModalBtn = $('<button class="modal-close">Close</button>');
-        $(pokemodal).append($closeModalBtn);
-        $closeModalBtn.click(hideModal);
-
-
-      var modalTitle = $('<h1>');
-        $(modalTitle).text(item.name);
-        $(modalTitle).addClass('modal-title');
-        $(pokemodal).append(modalTitle);
-
-    /*  var modalWebsite = $('<img>');
-        $(modalImg).addClass('poke-img');
-        $(modalImg).attr('src',item.imageUrl);
-        $(pokemodal).append(modalImg); */
-
-      var breweryState = $('<p>');
-        $(breweryState).text('State: ' + item.state);
-        $(breweryState).addClass('modal-para');
-        $(brewerymodal).append(State);
-
-        var breweryCity = $('<p>');
-          $(breweryCity).text('City: ' + item.city);
-          $(breweryCity).addClass('modal-para');
-          $(brewerymodal).append(City);
-    }
-  //Function to hide modal
-    function hideModal() {
-      //var $modalContainer = document.querySelector('#modal-container');
-      var modalContainer = $('#modal-container');
-      $modalContainer.removeClass('is-visible');
-    }
-
-    //Function to show details of each Pokemon
-    function showDetails(item) {
-      breweryRepository.loadDetails(item).then(function() {
-        console.log(item);
-        return item;
-      }).then(function(item) {
-        console.log('TCL: showDetails -> item', item);
-        showModal(item);
-      });
-    }
-
-    $modalContainer.on('keydown', function(e) {
-      if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
-        hideModal();
-      }
+        item.state = details.state;
+      item.types = Object.keys(details.types);
+    }).catch(function (error) {
+          console.error(error);
     });
+  }
 
-    $modalContainer.on('click', (e) => {
-      // Since this is also triggered when clicking INSIDE the modal
-      // We only want to close if the user clicks directly on the overlay
-      var target = e.target;
-      console.log('TCL: breweryRepository -> target', target);
-      var $modalClose = $('.modal-close');
-      if (target === $modalContainer || $modalClose) {
-        hideModal();
-      }
-    })
+/*Model Definition With Jquery Start*/
+function showModal(item){
 
-    return{
+    var modalContainer = $('#modal-container');
+      $(modalContainer).text('');
+      $(modalContainer).addClass('is-visible');
+
+    var brewmodal = $('<div>');
+      $(brewmodal).addClass('modal');
+      $(modalContainer).append(brewmodal);
+
+    var $closeModalBtn = $('<button class="modal-close">Close</button>');
+      $(brewmodal).append($closeModalBtn);
+      $closeModalBtn.click(hideModal);
+
+
+    var modalTitle = $('<h1>');
+      $(modalTitle).text(item.name);
+      $(modalTitle).addClass('modal-title');
+      $(brewmodal).append(modalTitle);
+
+    /*var modalImg = $('<img>');
+      $(modalImg).addClass('brew-img');
+      $(modalImg).attr('src',item.imageUrl);
+      $(pokemodal).append(modalImg);*/
+
+    var brewCity = $('<p>');
+      $(brewCity).text('City: ' + item.city);
+      $(brewCity).addClass('modal-para');
+      $(brewmodal).append(brewCity);
+
+    var brewState = $('<p>');
+      $(brewState).text('State: ' + item.state);
+      $(brewState).addClass('modal-para');
+      $(brewmodal).append(brewState);
+  }
+
+function hideModal() {
+    var modalContainer = $('#modal-container');
+    $(modalContainer).removeClass('is-visible');
+  }
+
+$(window).keydown(function(event) {
+    var modalContainer =$('#modal-container');
+    if (
+      event.key === 'Escape' &&
+      $(modalContainer).containsClass('is-visible')
+    ) {
+      hideModal();
+    }
+  });
+
+  var modalContainer = document.querySelector('#modal-container');
+  modalContainer.addEventListener('click', function(event) {
+    var target = event.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+/*Model Definition With Jquery End*/
+return {  /*Return All Previous Function In Order To Be Available Outside Of IIFE */
       add: add,
-      getAll: getAll,
+      catchAll: catchAll,
       addListItem: addListItem,
-      showDetails: showDetails,
       loadList: loadList,
-      loadDetails: loadDetails,
+      //loadDetails: loadDetails,
       showModal: showModal,
       hideModal: hideModal
-    };
-  })();
+  };
+})();
 
-
-  //Creates list of Pokemon with Pokemon's name on the button
-  breweryRepository.loadList().then(function() {
-    //Create a reusable modal once
-    breweryRepository.showModal();
-    // Now the data is loaded!
-    breweryRepository.getAll().forEach(function(brewery){
-      breweryRepository.addListItem(brewery);
-    });
+breweryRepository.loadList().then(function() {
+  "use strict";
+  breweryRepository.catchAll().forEach(function(brewery){
+    breweryRepository.addListItem(brewery);
   });
+});
